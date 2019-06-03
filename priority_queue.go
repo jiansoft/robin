da530b0c7dc0dadfbe3e032429e54dbcd8cd80cd
Eddie Chen <ez3608@yahoo.com.tw>
2019-06-03 22:43:32 +0800

    1. Add PriorityQueue

---
+++ priority_queue.go
@@ -0,0 +1,90 @@
+package robin
+
+import (
+	"container/heap"
+	"sync"
+)
+
+var (
+	lock sync.Mutex
+)
+
+type Item struct {
+	Value    interface{}
+	Priority int64
+	Index    int
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+// ie. the 0th element is the lowest value
+type PriorityQueue []*Item
+
+func NewPriorityQueue(capacity int) PriorityQueue {
+	return make(PriorityQueue, 0, capacity)
+}
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Priority < pq[j].Priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+	//heap.Push(&pq, item)
+	n, c := len(*pq), cap(*pq)
+	if n >= c {
+		npq := make(PriorityQueue, n, c*2)
+		copy(npq, *pq)
+		*pq = npq
+	}
+
+	*pq = (*pq)[0 : n+1]
+	item := x.(*Item)
+	item.Index = n
+	(*pq)[n] = item
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	lock.Lock()
+	defer lock.Unlock()
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.Index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority  of an Item in the queue.
+func (pq *PriorityQueue) Update(item *Item) {
+	lock.Lock()
+	defer lock.Unlock()
+	heap.Fix(pq, item.Index)
+}
+
+func (pq *PriorityQueue) TryDequeue(limit int64) (*Item, bool) {
+	lock.Lock()
+	defer lock.Unlock()
+	if pq.Len() == 0 {
+		return nil, false
+	}
+
+	item := (*pq)[0]
+	if item.Priority > limit {
+		return nil, false
+	}
+
+	heap.Remove(pq, 0)
+
+	return item, true
+}
